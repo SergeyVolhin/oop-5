18cfb2a17dc9a43917773464c7a44549f4c004f0
serg.volhin2014@yandex.ru <serg.volhin2014@yandex.ru>
2020-08-31 10:32:49 +0500

    111

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooop_5
+{
+    class Program
+    {
+        class Person
+        {
+            private string name;
+            private string surname;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string Surname
+            {
+                get { return surname; }
+                set { surname = value; }
+            }
+
+            public void Display()
+            {
+                Console.WriteLine(Name + " | " + Surname);
+            }
+
+            public Person(string name, string surname)
+            {
+                this.name = name;
+                this.surname = surname;
+            }
+        }
+        class Employee : Person
+        {
+            private string company;
+
+            public Employee(string name, string surname, string comp)
+                : base(name, surname)
+            {
+                company = comp;
+            }
+
+            public string Company
+            {
+                get { return company; }
+                set { company = value; }
+            }
+            public void DisplayE()
+            {
+                Console.WriteLine(Name + " | " + Surname + " | " + Company);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person p = new Person("Сергей", "Вольхин");
+            Employee emp = new Employee("Людмила", "Добролюбова", "ИСиП-330");
+
+            p.Display();
+            emp.DisplayE();
+            Console.ReadKey();
+        }
+    }
+}
